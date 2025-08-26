35ac180ea2d5ea7f1249e0fd6c20ad25175a52c4
Hanzala Khattak <muhammadhanzalahkhan1@gmail.com>
2025-08-26 12:49:01 +0500

    Routes updated

--- routes/UserRouters.js
+++ routes/UserRouters.js
@@ -2,7 +2,37 @@ import express from "express";
 const router = express.Router();
 import {createNewUser,getAllUser,deleteUser,updateUser,getSpecificData,authRoute} from "../controllers/UserControllers2.js";
 
-// router.get('/getAllUser',getAllUser);
+// routes/UserRoute.js
+/**
+* @swagger
+* components:
+* schemas:
+* User:
+* type: object
+* required:
+* - name
+* - email
+* - password
+* properties:
+* id:
+* type: string
+* description: The user ID
+* name:
+* type: string
+* description: User's full name
+* email:
+* type: string
+* format: email
+* description: User's email address
+* password:
+* type: string
+* description: User's password
+* example:
+* id: 60c72b2f9b1e8e001c8e4b8a
+* name: John Doe
+* email: john@example.com
+* password: hashedpassword
+*/
 router.post('/postUser',createNewUser); //For Data Creation
 router.get('/getUser',getAllUser); // For Getting Data
 router.delete('/deleteUser/:UserId',deleteUser); // For Deleting Data
